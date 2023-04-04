c5b075cef7a3645dd5f710e5c194b27bde219e43
Eirlys <100836558+SerenityEirlys@users.noreply.github.com>
2023-04-04 18:34:16 +0700

    Add files via upload

---
+++ BeeTV-Bot/Commands/API_RIOT.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeTV_Bot.Commands
+{
+    public class API_RIOT : BaseCommandModule
+    {
+        [Command("RiotAPI")]
+        public async Task RiotAPICommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Hello");
+        }
+    }
+}
